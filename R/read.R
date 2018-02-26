aedced7f111ba7e87d9b251c0ea3fd7fb44283da
Duncan Temple Lang <duncan@r-project.org>
2018-02-26 13:40:46 -0800

    basics working for reading the code and then creating the long data frame with functions used for each line, so line numbers are repeated if there are multiple functions used on that line

---
+++ R/read.R
@@ -0,0 +1,77 @@
+#
+# Start with a CSV file or a history file
+#
+#
+
+readCode =
+function(f, ...)
+    UseMethod("readCode")
+
+readCode.data.frame =
+function(f, ...)
+{
+    isComment = sapply(f[[2]], function(x) length(x[[1]])) == 0
+    f[[2]][isComment] = lapply(f[[1]][isComment], Comment)
+    f[[2]][!isComment] = lapply(f[[2]][!isComment], `[[`, 1)
+    class(f[[2]]) = "list"
+    f[[2]]
+}
+
+
+readCode.character =
+function(f, ...)
+{
+    # This is for a regular history file, not a CSV file.
+    # readCSV() for that.
+    if(grepl("\\.csv$", f)) {
+        tmp = read.csv(f, header = FALSE, stringsAsFactors = FALSE)
+        e = evaluate::parse_all(tmp[[2]])
+    } else
+        e = evaluate::parse_all(file(f))
+    
+    readCode(e, ...)
+}
+
+Comment =
+function(e)
+{
+  structure(e, class = "Comment")
+}
+
+
+longDF =
+    #
+    # Takes the expressions from readCode()
+    #
+    # id - identifier for the file.
+    #   If not provided, no column for id is created.
+    #
+function(exprs, exprInfo = lapply(exprs, getInputs), id = character(), ...)    
+{
+    #    ans = data.frame(comment = rep(NA, length(exprs)))
+
+    tmp = lapply(seq(along = exprInfo),
+                  function(i) expandLine(exprInfo[[i]], i))
+    
+    ans = do.call(rbind, tmp)
+
+    if(length(id)) 
+        ans$id = rep(id, nrow(ans))
+
+    ans
+}
+
+expandLine =
+function(info, lineNum)
+{
+    ans = if(is(info@code, "Comment")) 
+              data.frame(lineNum = lineNum, comment = unclass(info@code), "functions" = as.character(NA), stringsAsFactors = FALSE)
+          else {
+              n = length(info@functions)  
+              data.frame(lineNum = rep(lineNum, ),
+                         comment = rep(as.character(NA), n),
+                         functions = names(info@functions),
+                         stringsAsFactors = FALSE)
+          }
+    ans
+}
