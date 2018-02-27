82ae8e71a61ae9c22b03b7f9830eaacdf0f781b9
Duncan Temple Lang <duncan@r-project.org>
2018-02-26 19:45:01 -0800

    handle inline comments. Also readCode(), by default, returns a data frame with 4 columns identifying the src, expr, inlinecomment and regular comment

--- R/read.R
+++ R/read.R
@@ -8,17 +8,42 @@ function(f, ...)
     UseMethod("readCode")
 
 readCode.data.frame =
-function(f, ...)
+function(f, asDataFrame = TRUE, ...)
 {
-    isComment = sapply(f[[2]], function(x) length(x[[1]])) == 0
+     # Clean up src to remove leading and trailing whitespace
+    f[[1]] = trim(f[[1]])
+     # remove a comment character with no content after it.
+    f[[1]] = gsub("#$", "", f[[1]])
+
+     # remove empty rows, i.e. the src or the expression is empty.
+     # in other words, keep any row for which either is non-empty.
+    elen = sapply(f[[2]], function(x) length(x[[1]]))
+    f = f[f[[1]] != "" | elen > 0,]
+
+     # Figure out the inline and the "pure" comments (i.e with no code)
+    hasCommentText = grepl("#[[:space:]]*[^[:space:]]", f[[1]])
+    isInlineComment = hasCommentText & grepl("[^[:space:]]", gsub("#.*", "", f[[1]]))
+    isComment = !isInlineComment & hasCommentText
+
+     # Extract the expression
+    i = sapply(f[[2]], function(x) is.expression(x) && length(x) == 1)
+    f[[2]][i] = lapply(f[[2]][i], `[[`, 1)    
+     # Convert any comment text to a Comment object in the expr
     f[[2]][isComment] = lapply(f[[1]][isComment], Comment)
-    f[[2]][!isComment] = lapply(f[[2]][!isComment], `[[`, 1)
-    class(f[[2]]) = "list"
-    f[[2]]
+
+    if(asDataFrame) {
+        f$isInlineComment = isInlineComment
+        f$isComment = !isInlineComment & hasCommentText
+        f
+    } else 
+        structure(f[[2]], class = "list")
 }
 
 
 readCode.character =
+    #
+    # read from a file
+    #
 function(f, ...)
 {
     # This is for a regular history file, not a CSV file.
@@ -33,6 +58,7 @@ function(f, ...)
 }
 
 Comment =
+    # Create a comment object.
 function(e)
 {
   structure(e, class = "Comment")
@@ -42,6 +68,9 @@ function(e)
 longDF =
     #
     # Takes the expressions from readCode()
+    # and arranges in long form with a row for each function called
+    # with potentially multiple rows corresponding to a single expression due to .
+    # multiple nested calls within that expression, e.g. sum(rnorm(x)).
     #
     # id - identifier for the file.
     #   If not provided, no column for id is created.
@@ -75,3 +104,6 @@ function(info, lineNum)
           }
     ans
 }
+
+trim = function (x) 
+gsub("(^[[:space:]]+|[[:space:]]+$)", "", x)
